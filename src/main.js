6672f9af8f47c35a89d9cb4b4a32bddf7b972aa2
herquez <miguelherquez@gmail.com>
2021-08-31 08:10:20 -0500

    se agrego la libreria pokedexpromisev2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-const API_URL = 'https://pokeapi.co/api/v2/';
+var Pokedex = require('pokedex-promise-v2');
+var P = new Pokedex();
 
-fetch(`${API_URL}pokemon/1`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    var pokemon = data
-    console.log("nombre: ", pokemon.name)
+// const API_URL = 'https://pokeapi.co/api/v2/';
+
+// fetch(`${API_URL}pokemon/1`)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//     var pokemon = data
+//     console.log("nombre: ", pokemon.name)
+//   });
+
+P.getPokemonByName('eevee') // with Promise
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log('There was an ERROR: ', error);
   });
